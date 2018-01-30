1917eda0afe767dacdafb43524096220b5805384
Chris McCormick <chris@mccormick.cx>
2018-01-30 12:14:13 +0800

    Fades etc.

--- src/museum_of_water/core.cljs
+++ src/museum_of_water/core.cljs
@@ -23,13 +23,13 @@
 
 (defn home-page [data audio-files image-files]
   (let [selected (get @state 1)]
-    [:div#app
+    [:div#app (comment (when selected {:class "hero"}))
      (when selected
-       [:div
+       [:div#detail
         [:audio {:src (str "media/MP3/" selected "_" (get audio-files selected)) :auto-play true :controls false}]
         [:img#hero {:src (str "media/Small JPEGS/" selected "L.jpg")}]
-        [:img#back {:src "img/chevron-circle-left.svg" :on-click (fn [] (reset! state nil))}]])
-     [:div {:class (if  @state "hidden" "")}
+        [:img#back {:src "img/chevron-circle-left.svg" :on-click (fn [ev] (reset! state nil))}]])
+     [:div {:class (if @state "hidden" "")}
       [:img#logo {:src "img/museum-of-water-wordmark.svg"}]
       [:div#bottles
        (for [d data]
@@ -50,7 +50,7 @@
           audio-files (into {} (map (fn [a] {(get a 1) (get a 2)}) (re-seq re-audio-links (<! (get-file "media/MP3/")))))
           image-files (map #(second %) (re-seq re-image-links (<! (get-file "media/Small JPEGS/"))))]
       ;(print data)
-      (print (get audio-files "778"))
+      ;(print (get audio-files "778"))
       ;(print image-files)
       (r/render [home-page data audio-files image-files] (.getElementById js/document "app")))))
 
