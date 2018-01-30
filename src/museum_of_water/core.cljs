74f64f5dfe5940498d6a9c30cee76e98a1fa4ade
Chris McCormick <chris@mccormick.cx>
2018-01-30 11:29:29 +0800

    Audio playing.

--- src/museum_of_water/core.cljs
+++ src/museum_of_water/core.cljs
@@ -7,9 +7,9 @@
   (:require-macros
     [cljs.core.async.macros :refer [go alt!]]))
 
-(def current (r/atom nil))
+(def state (r/atom nil))
 
-(def re-audio-links #"href=\"/media/MP3/._(.*?)\"")
+(def re-audio-links #"href=\"/media/MP3/([0-9]+)_(.*?)\"")
 (def re-image-links #"href=\"/media/Small%20JPEGS/([^\.]*?).jpg\"")
 
 (defn get-file [url]
@@ -22,21 +22,23 @@
 ;; Views
 
 (defn home-page [data audio-files image-files]
-  [:div#app 
-   (when @current
-     [:div
-      [:img#hero {:src (str "media/Small JPEGS/" (get @current 1) "L.jpg")}]
-      [:img#back {:src "img/chevron-circle-left.svg" :on-click #(reset! current nil)}]])
-   [:div {:class (if @current "hidden" "")}
-    [:img#logo {:src "img/museum-of-water-wordmark.svg"}]
-    [:div#bottles
-     (for [d data]
-       (with-meta
-         [:span.thumbnail
-          [:img {:src (str "media/thumbnails/" (get d 1) "L.jpg")
-                 :on-click (fn [ev] (print d) (reset! current d))}]
-          (get d 1)]
-         {:key (get d 1)}))]]])
+  (let [selected (get @state 1)]
+    [:div#app
+     (when selected
+       [:div
+        [:audio {:src (str "media/MP3/" selected "_" (get audio-files selected)) :auto-play true :controls false}]
+        [:img#hero {:src (str "media/Small JPEGS/" selected "L.jpg")}]
+        [:img#back {:src "img/chevron-circle-left.svg" :on-click (fn [] (reset! state nil))}]])
+     [:div {:class (if  @state "hidden" "")}
+      [:img#logo {:src "img/museum-of-water-wordmark.svg"}]
+      [:div#bottles
+       (for [d data]
+         (with-meta
+           [:span.thumbnail
+            [:img {:src (str "media/thumbnails/" (get d 1) "L.jpg")
+                   :on-click (fn [ev] (reset! state d))}]
+            (get d 1)]
+           {:key (get d 1)}))]]]))
 
 ;; -------------------------
 ;; Initialize app
@@ -45,12 +47,12 @@
   ; at startup load the asset lists
   (go
     (let [data (filter #(= (get % 6) "Yes") (csv/parse (<! (get-file "media/data-csv.txt"))))
-          audio-files (map #(second %) (re-seq re-audio-links (<! (get-file "media/MP3/"))))
+          audio-files (into {} (map (fn [a] {(get a 1) (get a 2)}) (re-seq re-audio-links (<! (get-file "media/MP3/")))))
           image-files (map #(second %) (re-seq re-image-links (<! (get-file "media/Small JPEGS/"))))]
-      (print data)
-      (print audio-files)
-      (print image-files)
-      (r/render [home-page data] (.getElementById js/document "app")))))
+      ;(print data)
+      (print (get audio-files "778"))
+      ;(print image-files)
+      (r/render [home-page data audio-files image-files] (.getElementById js/document "app")))))
 
 (defn init! []
   (mount-root))
